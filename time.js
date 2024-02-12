b57f3e0c7165228d875b921a34c3d77b1a1ab5bf
danielle-gan <danielle.gan@uconn.edu>
2024-02-12 15:44:03 -0500

    styles 2 with some nice rays :) I'll have to get rid of some tailwind so I understand the codepen better

---
+++ time.js
@@ -0,0 +1,59 @@
+
+
+function Time() {
+
+    // Creating object of the Date class
+    var date = new Date();
+
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+
+
+    // Variable to store AM / PM
+    var period = "";
+
+    // Assigning AM / PM according to the current hour
+    if (hour >= 12) {
+        period = "PM";
+    } else {
+        period = "AM";
+    }
+
+    // Converting the hour in 12-hour format
+    if (hour == 0) {
+        hour = 12;
+    } else {
+        if (hour > 12) {
+            hour = hour - 12;
+        }
+    }
+
+    // Updating hour, minute
+    // if they are less than 10
+    hour = update(hour);
+    minute = update(minute);
+
+    // Adding time elements to the div
+    document.getElementById("digital-clock").innerText = hour + " : " + minute + " " + period;
+
+    // Set Timer to 1 sec (1000 ms)
+    setTimeout(Time, 10000);
+}
+
+// Function to update time elements if they are less than 10
+// Append 0 before time elements if they are less than 10
+function update(t) {
+    if (t < 10) {
+        return "0" + t;
+    }
+    else {
+        return t;
+    }
+}
+
+Time();
+
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+document.getElementById("current_date").innerHTML = new Date().toLocaleDateString('en-US', options);
+
+

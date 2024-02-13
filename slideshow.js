519bc2f4817b2ea34dcbe55a934c8350fd477a11
danielle-gan <danielle.gan@uconn.edu>
2024-02-13 11:57:24 -0500

    added opacity transitions to the slideshow

--- slideshow.js
+++ slideshow.js
@@ -1,13 +1,12 @@
 // Array of image sources
-const imageSources = ["pantone.jpg", "valentines.png", "yet-another-image.jpg"];
+const imageSources = ["pantone.jpg", "valentines.png", "2024Q1_NewHires.jpg"];
 
 // Array of header texts
-const headerTexts = ["2024 Color Of The Year", "Upcoming Holiday:", "Yet Another Header"];
+const headerTexts = ["2024 Color Of The Year", "Upcoming Holiday:", "Say hello to our new employees!"];
 
-// Initial index
 let currentIndex = 0;
 
-// Function to update image and header
+// Function to update image and header with fade-in effect
 function updateRotatingImages() {
     // Get elements
     const rotatingImg = document.getElementById("rotating-img");
@@ -20,6 +19,11 @@ function updateRotatingImages() {
     // Update header text
     rotatingHeader.textContent = headerTexts[currentIndex];
 
+    // Set different transition durations for image and header
+    rotatingImg.style.animation = 'fadeInFromNone 4s ease-in-out infinite'; // Example: 2 seconds for the image
+    rotatingHeader.style.animation = 'fadeInFromNone 4s ease-in-out infinite'; // Example: 1 second for the header
+
+
     // Increment index or reset to 0 if reached the end
     currentIndex = (currentIndex + 1) % imageSources.length;
 }
@@ -28,4 +32,4 @@ function updateRotatingImages() {
 updateRotatingImages();
 
 // Set interval to change images every 3 seconds (adjust as needed)
-setInterval(updateRotatingImages, 1000);
+setInterval(updateRotatingImages, 4000);

4366f9cdc480337add3534a83fbaf3ad3d5a068c
danielle-gan <danielle.gan@uconn.edu>
2024-02-14 16:43:43 -0500

    weather widget with icons is working, need to work on functionality for snow and rain divs being added dynamically

--- weather.js
+++ weather.js
@@ -9,6 +9,29 @@ let apiKey = "appid=052c1a4835e3512057e216db8dc1211f";/*placeholder*/
 let apiURL = "https://" + queryURL + lat + lon + apiOptions + apiKey;
 
 
+//HTML IDs TO GRAB 
+var temp = document.getElementById("CurrentTemp");
+
+var tempmin = document.getElementById("CurrentMin");
+var tempmax = document.getElementById("CurrentMax");
+var tempicon = document.getElementById("CurrentIcon");
+
+var tomorrowmin = document.getElementById("TomorrowMin");
+var tomorrowmax = document.getElementById("TomorrowMax");
+var tomorrowicon = document.getElementById("TomorrowIcon");
+
+var day2min = document.getElementById("Day2Min");
+var day2max= document.getElementById("Day2Max");
+var day2icon = document.getElementById("Day2Icon");
+
+var day3min = document.getElementById("Day3Min");
+var day3max= document.getElementById("Day3Max");
+var day3icon = document.getElementById("Day3Icon");
+
+minArr = [tempmin, tomorrowmin, day2min, day3min];
+maxArr = [tempmax, tomorrowmax, day2max, day3max];
+iconArr = [tempicon, tomorrowicon, day2icon, day3icon]; 
+
 fetch(apiURL)
 .then((response) => {
     if (!response.ok) {
@@ -44,25 +67,42 @@ fetch(apiURL)
     let day3Min = Math.round(day3.temp_min);
     let day3Max = Math.round(day3.temp_max);
 
-    console.log("It's: " + day3Desc2 +" 3 days from now");
-
     // Some arrays to hold things:
-    let minsArr = [currentMin, tomorrowMin, day2Min, day3Min];
-    let maxsArr = [currentMax, tomorrowMax, day2Max, day3Max];
-    let descsArr = [currentDesc, tomorrowDesc, day2Desc, day3Desc]
-    let descs2Arr = [currentDesc2, tomorrowDesc2, day2Desc2, day3Desc2]
+    let minsAPIArr = [currentMin, tomorrowMin, day2Min, day3Min];
+    let maxsAPIArr = [currentMax, tomorrowMax, day2Max, day3Max];
+    let descsAPIArr = [currentDesc, tomorrowDesc, day2Desc, day3Desc]
+    let descs2APIArr = [currentDesc2, tomorrowDesc2, day2Desc2, day3Desc2]
+
+    temp.innerHTML = currentTemp + "&deg;F"; 
+
+minArr.forEach((element, index) => {
+    element.innerHTML = "Lo " + minsAPIArr[index] + "&deg;F";
+})
+
+maxArr.forEach((element, index) => {
+    element.innerHTML = "Hi " + maxsAPIArr[index] + "&deg;F";
+})
+
+iconArr.forEach((element, index) => {
+    if (descs2APIArr[index] == "few clouds") {
+      element.src = "./weather_icons/light_clouds.png";
+    } else {
+      element.src = "./weather_icons/" + descsAPIArr[index] + ".png";
+    }
+  });
+
+
+
 
 // TEST to make sure I'm getting the info I need :) 
-    // function logAll(i) {
-    //     console.log(i);
-    // }
-
-    // minsArr.forEach(logAll);
-    // maxsArr.forEach(logAll);
-    // descsArr.forEach(logAll);
-    // descs2Arr.forEach(logAll);
+    function logAll(i) {
+        console.log(i);
+    }
 
 })
 .catch((error) => {
     console.error("Error fetching data:", error);
 });
+
+
+

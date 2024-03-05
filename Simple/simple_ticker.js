cceb36808d5fc402f89f0626b6a230ac52d0bdf6
danielle-gan <danielle.gan@uconn.edu>
2024-03-05 12:05:05 -0500

    simple layout finished

---
+++ Simple/simple_ticker.js
@@ -0,0 +1,29 @@
+const funFacts = [
+    "Postcards were one of the most popular forms of direct mail marketing in the late 19th & early 20th centuries.",
+    "Direct mail marketing dates back to 1000 B.C. when an Egyptian landowner sent an ad on a piece of papyrus",
+    "Johannes Gutenberg developed the printing press in 1440",
+    "ZIP codes were introduced in 1963 to improve the efficiency of direct mail delivery",
+    'The term "Direct Marketing" was coined by Lester Wunderman in 1967',
+    "Benjamin Franklin created one of the first mail-order catalogs",
+    "Direct mail can have an ROI of over 29%",
+    "Personalized mail, like using the recipient's name and tailored content, tends to yield higher response rates",
+    "Modern direct mail often uses variable data printing to personalize messages and offers for individual recipients",
+    "According to the USPS, 77% of people sort through their mail upon retrieval"
+  ];
+
+  
+  let index = 0;
+
+  function updateTicker() {
+    const tickerText = document.getElementById("ticker-text");
+    tickerText.textContent = funFacts[index];
+    tickerText.style.animation = 'fadeInFromNone 7.5s ease-in-out infinite';
+
+    index = (index + 1) % funFacts.length;
+
+  }
+
+  // Start the ticker
+  updateTicker();
+
+  setInterval(updateTicker, 8000);
